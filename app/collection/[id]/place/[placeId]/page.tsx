ba58d259123fc940034aab7b0acd813cc4c8eac4
projectmiluju <dnjsdyd0712@naver.com>
2026-02-14 22:21:18 +0900

    feat(placeDetail): 장소 상세 페이지에 NavigationBar 추가 및 스타일 수정 [WAPO-176]

--- app/collection/[id]/place/[placeId]/page.tsx
+++ app/collection/[id]/place/[placeId]/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/layout/Header";
+import NavigationBar from "@/components/layout/NavigationBar";
 
 const PlaceDetailPage = () => {
   return (
-    <div>
+    <div className="relative min-h-screen">
       <Header showBackButton leftBtnBgVariant="glass" />
+      <NavigationBar className="fixed bottom-0 left-0 right-0 z-50" />
     </div>
   );
 };
