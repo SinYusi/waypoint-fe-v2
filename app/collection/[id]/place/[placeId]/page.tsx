4b7be390461a65ecac5f7b784eef4beca41bfaf4
projectmiluju <dnjsdyd0712@naver.com>
2026-02-14 22:25:24 +0900

    feat(placeDetail): 장소 상세 페이지의 Header 구조 수정 및 스타일 개선 [WAPO-176]

--- app/collection/[id]/place/[placeId]/page.tsx
+++ app/collection/[id]/place/[placeId]/page.tsx
@@ -4,7 +4,14 @@ import NavigationBar from "@/components/layout/NavigationBar";
 const PlaceDetailPage = () => {
   return (
     <div className="relative min-h-screen">
-      <Header showBackButton leftBtnBgVariant="glass" />
+      <div className="fixed top-0 left-0 right-0 w-full aspect-5/3 bg-gray-200 z-0">
+        <div className="absolute top-0 left-0 right-0 z-10">
+          <Header showBackButton leftBtnBgVariant="glass" />
+        </div>
+      </div>
+      <div className="relative pt-[60%]">
+        {/* 스크롤되는 콘텐츠 영역 */}
+      </div>
       <NavigationBar className="fixed bottom-0 left-0 right-0 z-50" />
     </div>
   );
