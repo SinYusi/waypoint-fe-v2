9f6322225fda64b895000f0c440b3ebf36d5dc30
projectmiluju <dnjsdyd0712@naver.com>
2026-02-15 01:40:16 +0900

    fix(Label): 수정 버튼 및 저장 버튼 기능 추가 [WAPO-176]

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,18 +1,22 @@
 import * as React from "react"
-import { Pencil } from "lucide-react"
+import { Pencil, Check } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
 type LabelProps = React.ComponentProps<"label"> & {
   required?: boolean
+  isEditing?: boolean
   onEdit?: () => void
+  onSave?: () => void
 }
 
 function Label({
   className,
   required,
+  isEditing,
   onEdit,
+  onSave,
   children,
   ...props
 }: LabelProps) {
@@ -32,7 +36,18 @@ function Label({
         {required && <span className="text-sky-500">*</span>}
       </label>
 
-      {onEdit && (
+      {isEditing && onSave && (
+        <Button
+          type="button"
+          variant="ghost"
+          size="S"
+          onClick={onSave}
+        >
+          <Check className="size-[18px]" />
+        </Button>
+      )}
+
+      {!isEditing && onEdit && (
         <Button
           type="button"
           variant="ghost"
