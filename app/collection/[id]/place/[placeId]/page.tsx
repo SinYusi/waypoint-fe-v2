085acb27e6a8412f882217864377461aea710008
projectmiluju <dnjsdyd0712@naver.com>
2026-02-14 22:18:31 +0900

    feat(placeDetail): 장소 상세 페이지 컴포넌트 추가 [WAPO-176]

---
+++ app/collection/[id]/place/[placeId]/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/layout/Header";
+
+const PlaceDetailPage = () => {
+  return (
+    <div>
+      <Header showBackButton leftBtnBgVariant="glass" />
+    </div>
+  );
+};
+
+export default PlaceDetailPage;
