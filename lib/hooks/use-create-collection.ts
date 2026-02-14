401160a6758a2a0010e7b69b3ce4e3364750c4a0
H-JuKyung <hjk000hjk17@gmail.com>
2026-02-15 02:08:43 +0900

    refactor(collection): useCreateCollection 에러 타입을 ProblemDetail로 명확화 [WAPO-178]

--- lib/hooks/use-create-collection.ts
+++ lib/hooks/use-create-collection.ts
@@ -11,16 +11,25 @@ import type {
   CreateCollectionRequest,
 } from "@/types/collection";
 import { createCollection } from "../api/collection";
+import { ProblemDetail } from "@/types/problem-detail";
 
 type Options = Omit<
-  UseMutationOptions<CollectionResponse, AxiosError, CreateCollectionRequest>,
+  UseMutationOptions<
+    CollectionResponse,
+    AxiosError<ProblemDetail>,
+    CreateCollectionRequest
+  >,
   "mutationFn"
 >;
 
 export const useCreateCollection = (options?: Options) => {
   const queryClient = useQueryClient();
 
-  return useMutation({
+  return useMutation<
+    CollectionResponse,
+    AxiosError<ProblemDetail>,
+    CreateCollectionRequest
+  >({
     mutationFn: createCollection,
     ...options,
     onSuccess: (data, variables, onMutateResult, context) => {
