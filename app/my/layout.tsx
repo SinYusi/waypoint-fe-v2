e84c6840d3b0b6517f36b3294bdb7b70f97e33e2
Yusi <yusi4781@naver.com>
2026-02-15 23:10:32 +0900

    feat(mypage): 마이페이지 레이아웃 추가 [WAPO-182]

---
+++ app/my/layout.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/layout/Header";
+import NavigationBar from "@/components/layout/NavigationBar";
+
+interface MypageLayoutProps {
+  children: React.ReactNode;
+}
+
+const MypageLayout = ({ children }: MypageLayoutProps) => {
+  return (
+    <div>
+      <Header showNotificationButton rightBtnBgVariant="ghost" />
+      {children}
+      <NavigationBar className="fixed bottom-0 left-0 right-0 z-50" />
+    </div>
+  );
+};
+
+export default MypageLayout;
