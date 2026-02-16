c8dfdc81daafc1bb4bf47800ef803e3a041be792
Yusi <yusi4781@naver.com>
2026-02-16 14:08:32 +0900

    feat(edit-page): 회원 정보 수정 페이지에서 회원 조회 API 연결 [WAPO-183]

--- app/my/edit/page.tsx
+++ app/my/edit/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import ProfileImage from "@/components/common/ProfileImage";
+import { useMe } from "@/lib/hooks/use-me";
+
 const EditMyInformationPage = () => {
-  return <div></div>;
+  const { data: me } = useMe();
+
+  const nickname = me?.nickname ?? "";
+  const picture = me?.picture ?? "";
+
 };
 
 export default EditMyInformationPage;
