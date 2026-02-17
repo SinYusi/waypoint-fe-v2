afef7c7d743f175a2053c08d482e9c31072f45e0
Yusi <yusi4781@naver.com>
2026-02-18 04:22:09 +0900

    feat(edit-my-page): 헤더 버튼 디자인에 맞게 수정 [WAPO-183]

--- app/my/edit/layout.tsx
+++ app/my/edit/layout.tsx
@@ -7,7 +7,12 @@ interface EditLayoutProps {
 const EditLayout = ({ children }: EditLayoutProps) => {
   return (
     <div>
-      <Header variant="center" title="회원 정보 수정" showBackButton />
+      <Header
+        variant="center"
+        title="회원 정보 수정"
+        showBackButton
+        leftBtnBgVariant="ghost"
+      />
       {children}
     </div>
   );
