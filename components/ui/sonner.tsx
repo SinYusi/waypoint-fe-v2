dfdb130ced92ff721adf1c63b9dd2f854bd472c3
H-JuKyung <hjk000hjk17@gmail.com>
2026-02-18 02:17:20 +0900

    style(sonner): 디자인에 맞게 Sonner 컴포넌트 스타일 커스텀 [WAPO-186]

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,12 +1,5 @@
 "use client";
 
-import {
-  CircleCheckIcon,
-  InfoIcon,
-  Loader2Icon,
-  OctagonXIcon,
-  TriangleAlertIcon,
-} from "lucide-react";
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
@@ -18,20 +11,43 @@ const Toaster = ({ ...props }: ToasterProps) => {
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       icons={{
-        success: <CircleCheckIcon className="size-4" />,
-        info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
-        loading: <Loader2Icon className="size-4 animate-spin" />,
+        success: null,
+        info: null,
+        warning: null,
+        error: null,
+        loading: null,
       }}
       style={
         {
           "--normal-bg": "var(--popover)",
+          "--success-bg": "var(--success)",
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
-          "--border-radius": "var(--radius)",
+          "--border-radius": "var(--radius-xl)",
         } as React.CSSProperties
       }
+      toastOptions={{
+        className: [
+          "!px-5 !py-3 lg:!py-4.5 !bg-popover",
+
+          // title & description (typo)
+          "[&_[data-title]]:!typography-body-sm-bold",
+          "[&_[data-description]]:!typography-body-sm-reg [&_[data-description]]:!text-muted-foreground",
+
+          // border (색상)
+          "[&[data-type=success]]:!border-success",
+          "[&[data-type=error]]:!border-destructive",
+
+          // title (색상)
+          "[&[data-type=success]_[data-title]]:!text-success",
+          "[&[data-type=error]_[data-title]]:!text-destructive",
+
+          // 버튼
+          "[&_[data-button]]:!bg-transparent",
+          "[&_[data-button]]:!p-0",
+          "[&_[data-button]]:!text-foreground",
+        ].join(" "),
+      }}
       {...props}
     />
   );
