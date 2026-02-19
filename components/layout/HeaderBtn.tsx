3c9e2159fc964cb202570103169238ec3dee7a49
H-JuKyung <hjk000hjk17@gmail.com>
2026-02-20 04:44:20 +0900

    style(header-button): 항상 cursor-pointer 적용 [WAPO-206]

--- components/layout/HeaderBtn.tsx
+++ components/layout/HeaderBtn.tsx
@@ -27,8 +27,7 @@ const HeaderBtn = ({
       aria-label={label}
       onClick={onClick}
       className={cn(
-        "rounded-full size-11 p-2.5",
-        onClick ? "cursor-pointer" : "cursor-not-allowed",
+        "rounded-full size-11 p-2.5 cursor-pointer",
         bgVariant === "solid" && "bg-[#FAFAFA]",
         bgVariant === "glass" && "bg-[#FAFAFA]/60 backdrop-blur-sm",
         bgVariant === "ghost" && "bg-transparent backdrop-blur-xl",
