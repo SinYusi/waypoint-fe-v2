a834097ba9e552d6e62583ba7adeb3cea6ec0074
projectmiluju <dnjsdyd0712@naver.com>
2026-02-21 07:43:51 +0900

    fix(PlanMode): 버튼 스타일 수정 및 클래스 순서 변경 [WAPO-219]

--- components/common/PlanMode.tsx
+++ components/common/PlanMode.tsx
@@ -25,10 +25,10 @@ export default function PlanMode({
 					<PlanModeIcon aria-hidden className="size-4 opacity-40" />
 				}
 				className={cn(
-					"h-8 w-full rounded-full border-border px-2.5 py-0.5 typography-nav-xl-bold text-foreground hover:text-foreground",
+					"h-8 w-full rounded-full px-2.5 py-0.5 typography-nav-xl-bold text-foreground hover:text-foreground",
 					isVariant2
-						? "bg-transparent hover:bg-transparent"
-						: "bg-background hover:bg-background",
+						? "border-transparent bg-transparent hover:bg-transparent"
+						: "border-border bg-background hover:bg-background",
 				)}
 			>{text}</Button>
 		</div>
