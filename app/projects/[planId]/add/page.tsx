192eb8b15276a3de04b77cdf3df95d2176ed4852
projectmiluju <dnjsdyd0712@naver.com>
2026-02-21 12:22:07 +0900

    feat(add-plan-page): 경로 수정 [WAPO-218]

--- app/projects/[planId]/add/page.tsx
+++ app/projects/[planId]/add/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Header from "@/components/layout/Header";
-import NavigationBar from "@/components/layout/NavigationBar";
 
 const AddPlanPage = () => {
 	return (
@@ -16,7 +15,6 @@ const AddPlanPage = () => {
 
 			<main className="flex flex-1 flex-col px-5 pt-16 pb-24" />
 
-			<NavigationBar className="fixed inset-x-0 bottom-0 z-10" />
 		</div>
 	);
 };
