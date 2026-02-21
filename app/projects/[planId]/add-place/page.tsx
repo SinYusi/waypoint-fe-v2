d5aa5f597722d421c3387830eb659d088dfa6bb7
projectmiluju <dnjsdyd0712@naver.com>
2026-02-21 12:39:55 +0900

    feat(add-plan-page): 탭 컴포넌트 추가 [WAPO-218]

--- app/projects/[planId]/add-place/page.tsx
+++ app/projects/[planId]/add-place/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Header from "@/components/layout/Header";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const AddPlanPage = () => {
 	return (
@@ -13,8 +14,24 @@ const AddPlanPage = () => {
 				className="fixed inset-x-0 top-0 z-10"
 			/>
 
-			<main className="flex flex-1 flex-col px-5 pt-16 pb-24" />
-
+			<main className="flex flex-1 flex-col pt-16 pb-24">
+				<Tabs defaultValue="saved">
+					<TabsList style="underline" fullWidth className="w-full px-5">
+						<TabsTrigger value="saved" style="underline" fullWidth>
+							보관한
+						</TabsTrigger>
+						<TabsTrigger value="search" style="underline" fullWidth>
+							장소 검색
+						</TabsTrigger>
+						<TabsTrigger value="free" style="underline" fullWidth>
+							자유 시간
+						</TabsTrigger>
+					</TabsList>
+					<TabsContent value="saved" className="px-5" />
+					<TabsContent value="search" className="px-5" />
+					<TabsContent value="free" className="px-5" />
+				</Tabs>
+			</main>
 		</div>
 	);
 };
