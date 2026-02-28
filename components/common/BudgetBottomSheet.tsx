927d472135e9f489b1382c014afb967dad450966
projectmiluju <dnjsdyd0712@naver.com>
2026-03-01 07:37:59 +0900

    feat(budget): BudgetBottomSheet에 높이 클래스 추가 [WAPO-248]

--- components/common/BudgetBottomSheet.tsx
+++ components/common/BudgetBottomSheet.tsx
@@ -119,6 +119,7 @@ function BudgetBottomSheet({
     <BottomSheet
       open={open}
       onOpenChange={onOpenChange}
+      className="h-168.75"
       header={
         hasTitle ? (
           <h2 className="mb-2 w-full text-center font-sans text-lg font-semibold leading-4 tracking-normal text-black">
