1c76502e4453da7e6400d5f0b8de07fcdd52fa3d
projectmiluju <dnjsdyd0712@naver.com>
2026-03-01 05:52:11 +0900

    fix(budget): '원' 텍스트의 스타일을 조정하여 정렬 개선 [WAPO-248]

--- components/card/BudgetPlaceCard.tsx
+++ components/card/BudgetPlaceCard.tsx
@@ -55,7 +55,7 @@ const BudgetPlaceCard = ({
                 <span className="typography-body-base h-6 whitespace-nowrap leading-6">
                   {item.cost.toLocaleString("ko-KR")}
                 </span>
-                <span className="typography-body-sm-reg h-5 w-3.25 leading-5">원</span>
+                <span className="typography-body-sm-reg h-6 leading-6">원</span>
               </div>
             </div>
           ))
