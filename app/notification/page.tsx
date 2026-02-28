825167d757f774867c759f9fb52aaa6067f019d6
projectmiluju <dnjsdyd0712@naver.com>
2026-02-28 13:07:21 +0900

    feat(NotificationPage): 알림 페이지 컴포넌트 추가 [WAPO-246]

---
+++ app/notification/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Header from "@/components/layout/Header";
+
+const NotificationPage = () => {
+  const handleReadAll = () => {};
+
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <Header
+        variant="center"
+        title="알림"
+        showBackButton
+        leftBtnBgVariant="ghost"
+        rightContent={
+          <button
+            className="h-10 px-1.5 py-2.5 rounded-2xl text-sm font-bold leading-5 text-[#757575]"
+            onClick={handleReadAll}
+          >
+            모두 지우기
+          </button>
+        }
+      />
+
+      <main className="flex-1 px-4 pb-10">
+        {/* TODO: 알림 목록 UI */}
+      </main>
+    </div>
+  );
+};
+
+export default NotificationPage;
